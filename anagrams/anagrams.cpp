069c44df0f7a3c46302d4bc22d6992a7406aaf5a
piotr <cielecki.piotr@gmail.com>
2023-01-12 19:10:54 +0100

    Refactoring: significantly shortened anagrams

--- anagrams/anagrams.cpp
+++ anagrams/anagrams.cpp
@@ -10,39 +10,22 @@ using std::set;
 using std::multiset;
 
 
-string _strip_sentence(const string sentence) {
-    string stripped_sentence = sentence;
-
-    std::remove_if(stripped_sentence.begin(), stripped_sentence.end(), [](unsigned char c){return !std::isalpha(c);});
-    std::transform(stripped_sentence.begin(), 
-                   stripped_sentence.end(), 
-                   stripped_sentence.begin(), 
-                   [](unsigned char c){return std::tolower(c);});
+static string strip(const string str) {
+    string stripped_sentence;
+    for(char c: str)
+        if(isalpha(c))
+            stripped_sentence += std::tolower(c);
     return stripped_sentence;
 }
 
-multiset<char> _strip_phrase(const string phrase) {
-    multiset<char> phrase_stripped;
-    for(char  c: phrase) {
-        if(std::isalpha(c))
-            phrase_stripped.insert(std::tolower(c));
-    }
-    return phrase_stripped;
-}
-
 string find_hidden_anagram(string sentence, string phrase) {
-    string sentence_stripped = _strip_sentence(sentence);
-    multiset<char> phrase_stripped = _strip_phrase(phrase);
+    string sentence_stripped = strip(sentence);
+    string phrase_stripped = strip(phrase);
     string hidden_anagram = "noutfond";
 
-    for(unsigned int frame_start_pos = 0; frame_start_pos < sentence.length() - phrase.length()+1; ++frame_start_pos) {
-        multiset<char> frame;
-        string substr = sentence_stripped.substr(frame_start_pos, phrase_stripped.size());
-        for(auto c: substr)
-            frame.insert(c);
-
-        if(frame == phrase_stripped) {
-            hidden_anagram = substr;
+    for(string::iterator it = sentence_stripped.begin(); it <= sentence_stripped.end()-phrase_stripped.size(); ++it) {
+        if(std::is_permutation(it, it+phrase_stripped.size(), phrase_stripped.begin(), phrase_stripped.end())) {
+            hidden_anagram = string(it, it+phrase_stripped.size());
             break;
         }
     }
