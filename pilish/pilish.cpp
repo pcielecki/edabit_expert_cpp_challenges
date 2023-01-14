2d5f3473e134d3db4cf863c5d6a9f79ff65facb7
piotr <cielecki.piotr@gmail.com>
2023-01-14 19:17:40 +0100

    minor refactoring of pilish strings

--- pilish/pilish.cpp
+++ pilish/pilish.cpp
@@ -1,35 +1,30 @@
 #include <string>
+#include <array>
 #include <vector>
 #include "pilish.hpp"
 
 using std::string;
 using std::vector;
-
-string translate_to_pilish2(const string &msg) {return msg; }
+using std::array;
 
 string translate_to_pilish(const string& msg) {
     if("" == msg)
         return "";
 
-    const vector<int> pi_digits = {3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5, 8, 9, 7, 9};
+    constexpr const array<int, 15> pi_digits = {3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5, 8, 9, 7, 9};
     string pilish_msg;
     unsigned int word_begin_idx = 0;
 
     for(unsigned int digit: pi_digits) {
         if(word_begin_idx >= msg.size())
             break;
-        else if(word_begin_idx)
-            pilish_msg += " ";
-
-        pilish_msg += msg.substr(word_begin_idx, digit);
-
-        if(word_begin_idx + digit > msg.size()) {
-            unsigned int n_missing_chars = word_begin_idx + digit - msg.size();
-            while(n_missing_chars--)
-                pilish_msg += msg[msg.size() - 1];
-        } 
+        pilish_msg += msg.substr(word_begin_idx, digit) + string(" ");
         word_begin_idx += digit;
     }
-    
+    pilish_msg.pop_back(); // strip the last " "
+
+    for(int n_missing_chars = word_begin_idx - msg.size(); n_missing_chars > 0; --n_missing_chars)
+            pilish_msg += pilish_msg.back();
     return pilish_msg;
 }
+
