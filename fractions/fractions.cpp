386d5b6f56d57c009999878fddbdcdc888268411
piotr <cielecki.piotr@gmail.com>
2023-02-06 20:13:28 +0100

    the fractions challenge works, but needs some refactoring

--- fractions/fractions.cpp
+++ fractions/fractions.cpp
@@ -1,2 +1,40 @@
+#include <vector>
+#include <algorithm>
+#include <numeric>
+#include <set>
+#include <utility>
+using std::vector;
+using std::pair;
+using std::set;
 
-unsigned int get_n_proper_fractions(unsigned int max_denominator) {return 0;}
+const vector<set<unsigned int>> dividers {
+    {}, //0
+    {},
+    {2},
+    {3},
+    {2, 4},
+    {5},
+    {2, 3, 6},
+    {7},
+    {2, 4, 8},
+    {3, 9},
+    {2, 5, 10}
+};
+
+unsigned int get_n_proper_fractions(unsigned int max_denominator) {
+    vector<unsigned int> denominators(max_denominator-1, 0);
+    std::iota(denominators.begin(), denominators.end(), 2);
+
+
+    unsigned int n_fractions = 0;
+    for(unsigned int den: denominators) {
+        for(unsigned int numerator = 1; numerator < den; ++numerator) {
+            vector<unsigned int> intersect;
+            std::set_intersection(dividers.at(numerator).begin(), dividers.at(numerator).end(),
+                                  dividers.at(den).begin(), dividers.at(den).end(),
+                                  std::back_inserter(intersect));
+            if(intersect.empty()) n_fractions++;
+        }
+    }
+    return n_fractions;
+}
