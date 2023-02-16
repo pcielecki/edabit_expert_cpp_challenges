b2c29dd89b09b23c2602e891a98602bdb904f4e5
piotr <cielecki.piotr@gmail.com>
2023-02-16 12:32:55 +0100

    solved the polynomials challenge

--- polynomials/polynomials.cpp
+++ polynomials/polynomials.cpp
@@ -1,7 +1,62 @@
 #include <string>
-using std::string;
+#include <cmath>
+#include <numeric>
+#include <vector>
+#include <algorithm>
+#include <utility>
 
-int evaluate_polynomial(string str, int x) {
-    return -1;
+using std::string;
+using std::vector;
+using std::pair;
+
+typedef int (*operator_function)(int, int);
+const vector<vector<pair<char, operator_function>>> operators_to_operations {
+    {{'+', [](int a, int b){return a+b;}}, {'-', [](int a, int b){return a-b;}}}, // level 0
+    {{'*', [](int a, int b){return a * b;}}, {'/', [](int a, int b){return a / b;}}}, // level 1
+    {{'^', [](int a, int b){return (int)std::pow(a, b);}}} // level 2
+};
+
+static void split_str_by_relevant_operators(string str, int level, vector<string>& operations) {
+    unsigned int brackets_level = 0;
+    for(char c: str) {
+        if('(' == c) ++brackets_level;
+        else if(')' == c) --brackets_level;
+        else if(any_of(operators_to_operations.at(level).begin(), operators_to_operations.at(level).end(), 
+                  [c](pair<char, operator_function> op_at_level){return c == op_at_level.first;}) && !brackets_level)
+            operations.push_back(string());
+        operations.back().push_back(c);
+    }
 }
 
+static operator_function get_f_op_by_op(char op, int level) {
+    return std::find_if(operators_to_operations.at(level).begin(), operators_to_operations.at(level).end(), 
+                        [op](pair<char, operator_function> p) {
+                            return p.first == op;
+    })->second;
+}
+
+static int evaluate(string str, int x, unsigned int level) {
+    if("x" == str) return x;
+    if(std::all_of(str.begin(), str.end(), [](char c) {return (c >= '0' && c <= '9');})) return std::stoi(str);
+    if('(' == str.front() && ')' == str.back()) return evaluate(string(str.begin()+1, str.end()-1), x, 0);
+
+    vector<string> operations {""};
+    split_str_by_relevant_operators(str, level, operations);
+
+    return std::accumulate(operations.begin()+1, operations.end(), evaluate(*operations.begin(), x, level+1), 
+                           [level, x](int partial_sum, string a) {
+                                auto f = get_f_op_by_op(a.front(), level);
+                                a.erase(a.begin());
+                                return f(partial_sum, evaluate(a, x, level+1));
+                           });
+}
+int evaluate_polynomial(string str, int x) {
+    if(std::count(str.begin(), str.end(), '(') != std::count(str.begin(), str.end(), ')'))
+        return -1;
+    
+    for(string::iterator it = str.begin(); it != str.end()-1; ++it) 
+        if(((std::isdigit(*it) || 'x' == *it || ')' == *it) && '(' == *(it+1)) || (std::isdigit(*it) && 'x' == *(it+1)))
+            str.insert(it+1, '*');
+
+    return evaluate(str, x, 0);
+}
--- polynomials/test_polynomials.cpp
+++ polynomials/test_polynomials.cpp
@@ -1,12 +1,17 @@
 #include <gtest/gtest.h>
 #include "polynomials.hpp"
 
-TEST(PolynomialsSuite, TestValidScenarios) {
+TEST(PolynomialsSuite, TestSimpleLevel1Scenario) {
     EXPECT_EQ(6, evaluate_polynomial("x+1", 5));
-    EXPECT_EQ(45, evaluate_polynomial("5x^2", 3));
-    EXPECT_EQ(10, evaluate_polynomial("(x(x+1))/2", 4));
 }
 
-TEST(PolynomialsSuite, OnInvalidStringReturnmin1) {
+TEST(PolynomialsSuite, TestLevels2And3Scenarios) {
+    EXPECT_EQ(45, evaluate_polynomial("5x^2", 3));
+}
+
+TEST(PolynomialsSuite, TestScenarioWithBrackets) {
+    EXPECT_EQ(10, evaluate_polynomial("(x(x+1))/2", 4));
+}
+TEST(PolynomialsSuite, TestInvalidString) {
     ASSERT_EQ(-1, evaluate_polynomial("4(x+3))/2",5));
 }
