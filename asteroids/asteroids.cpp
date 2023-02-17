dc6559881f06c0c55cf8b8dba2c119dc49a582ee
piotr <cielecki.piotr@gmail.com>
2023-02-17 13:57:56 +0100

    solved the asteroids challenge

--- asteroids/asteroids.cpp
+++ asteroids/asteroids.cpp
@@ -1,6 +1,19 @@
 #include <vector>
+#include <cmath>
+#include <algorithm>
 using std::vector;
+using std::abs;
 
 vector<int> calculate_asteroids(vector<int> asteroids) {
-    return {};
+    for(vector<int>::iterator it = asteroids.begin(); it < asteroids.end()-1;) {
+        if(*it > 0 && *(it+1) < 0) {
+            if(-*it == *(it+1))
+                asteroids.erase(it, it+2);
+            else
+                asteroids.erase(*it < -*(it+1)? it : it+1);
+            it = asteroids.begin();
+        }
+        else ++it;
+    }
+    return asteroids;
 }
