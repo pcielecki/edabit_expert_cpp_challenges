404c3dcaf80044dc4faab4ff6f41d19f334c4bdb
piotr <cielecki.piotr@gmail.com>
2023-02-24 15:10:53 +0100

    fixed compiler warnings

--- s_string/s_string.cpp
+++ s_string/s_string.cpp
@@ -7,14 +7,14 @@ using std::vector;
 
 class Combinator{
 public:
-    Combinator(string all_chars, int wd_len) : m_all_chars(all_chars), m_indices(vector<int>(wd_len, 0)) {}
+    Combinator(string all_chars, int wd_len) : m_indices(vector<unsigned int>(wd_len, 0)), m_all_chars(all_chars) {}
     string get() { 
         string wd;
         for(int i: m_indices) wd.push_back(m_all_chars.at(i));
         return wd;
     }
     bool next() {
-        if(std::all_of(m_indices.begin(), m_indices.end(), [this](int a){return a == this->m_all_chars.size()-1;})) 
+        if(std::all_of(m_indices.begin(), m_indices.end(), [this](unsigned int a){return a == this->m_all_chars.size()-1;})) 
             return false;
         else {
             ++m_indices.at(0);
@@ -30,19 +30,20 @@ public:
         return true;
     }
 private:
-    vector<int> m_indices;
+    vector<unsigned int> m_indices;
     const string m_all_chars;
 };
     
 string get_kth_string(string chars, unsigned int max_size, unsigned int k) {
     std::sort(chars.begin(), chars.end());
     vector<string> sstrings;
-    for(int wd_len = 1; wd_len <= max_size; ++wd_len) {
+    for(unsigned int wd_len = 1; wd_len <= max_size; ++wd_len) {
         Combinator c(chars, wd_len);
         do {
             sstrings.push_back(c.get());
         } while (c.next());
     }
-    std::sort(sstrings.begin(), sstrings.end());
-    return sstrings.at(k-1);
+    vector<string>::iterator nth = sstrings.begin()+k-1;
+    std::nth_element(sstrings.begin(), nth , sstrings.end());
+    return *nth;
 }
