c0bc50d010ba7b3e64263c86105a758cdc532037
piotr <cielecki.piotr@gmail.com>
2023-02-25 18:18:51 +0100

    solved the casino challenge

--- casino/casino.cpp
+++ casino/casino.cpp
@@ -1,6 +1,16 @@
 #include <string>
+#include <algorithm>
 using std::string;
 
 string security(string s) {
-    return "";
+    s.erase(std::remove(s.begin(), s.end(), 'x'));
+
+    const string alarming_seq1 {"T$"};
+    const string alarming_seq2 {"$T"};
+
+    if(s.end() != std::search(s.begin(), s.end(), alarming_seq1.begin(), alarming_seq1.end())
+       || s.end() != std::search(s.begin(), s.end(), alarming_seq2.begin(), alarming_seq2.end()))
+        return "ALARM!";
+    else 
+        return "Safe";
 }
